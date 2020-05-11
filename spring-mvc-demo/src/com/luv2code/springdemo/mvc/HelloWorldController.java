19c062586496a0021ca4d76b6adb3a367ce4dbd1
Perego Daniele <peregopr@gmail.com>
2020-05-11 12:16:28 +0200

    Aggiornato progetto Spring (lezione 139)
    Arrivato fino a lezione 139 Udemy Sprring

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -5,8 +5,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/hello")
 public class HelloWorldController {
 
 	// need a controller method to show the initial HTML form
@@ -42,4 +44,20 @@ public class HelloWorldController {
 		return "helloworld";
 	}
 	
+	// new version
+	@RequestMapping("/processFormVersionThree")
+	public String processFormVersionThree(@RequestParam("studentName") String theName, 
+			Model model) {
+		
+		// convert the data to all Uppercase
+		theName = theName.toUpperCase();
+		
+		// create the message
+		String result = "Hey My Friend from v3! " + theName;
+		
+		// add message to the model
+		model.addAttribute("message", result);
+		
+		return "helloworld";
+	}
 }
---
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/Student.java
@@ -0,0 +1,96 @@
+package com.luv2code.springdemo.mvc;
+
+import java.util.LinkedHashMap;
+
+public class Student {
+
+	private String firstName;
+	private String lastName;
+	
+	private String country;
+	
+	private LinkedHashMap<String, String> countryOptions;
+	
+	private String favoriteLanguage;
+	
+	private LinkedHashMap<String, String> favoriteLanguageOptions; 
+	
+	private String[] operatingSystems;
+	
+	public Student() {
+		
+		// populate country options: used ISO coiuntry code
+		countryOptions = new LinkedHashMap<>();
+		
+		countryOptions.put("IT","Italy");
+		countryOptions.put("BR","Brazil");
+		countryOptions.put("FR","France");
+		countryOptions.put("DE","Germany");
+		countryOptions.put("IN","India");
+		countryOptions.put("US","United States of America");
+		
+		 // populate favorite language options
+        favoriteLanguageOptions = new LinkedHashMap<>();
+
+        // parameter order: value, display label
+        favoriteLanguageOptions.put("Java", "Java");
+        favoriteLanguageOptions.put("C#", "C#");
+        favoriteLanguageOptions.put("PHP", "PHP");
+        favoriteLanguageOptions.put("Ruby", "Ruby");    
+        favoriteLanguageOptions.put("C++", "C++");  
+        favoriteLanguageOptions.put("Assembly", "Assembly");  
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
+	}
+
+	public String getFavoriteLanguage() {
+		return favoriteLanguage;
+	}
+
+	public void setFavoriteLanguage(String favoriteLanguage) {
+		this.favoriteLanguage = favoriteLanguage;
+	}
+	
+	public LinkedHashMap<String, String> getFavoriteLanguageOptions() {
+	        return favoriteLanguageOptions;
+	}
+
+	public String[] getOperatingSystems() {
+		return operatingSystems;
+	}
+
+	public void setOperatingSystems(String[] operatingSystems) {
+		this.operatingSystems = operatingSystems;
+	}
+	
+	
+	
+	
+	
+}
