b21902263f688def0f3b88470b7809ca1ec7aafc
Perego Daniele <peregopr@gmail.com>
2020-05-20 13:36:39 +0200

    Aggiunta barra di ricerca nella schermata inizale
    Arrivato a lezione 315

--- web-customer-tracker/src/com/luv2code/springdemo/service/CustomerServiceImpl.java
+++ web-customer-tracker/src/com/luv2code/springdemo/service/CustomerServiceImpl.java
@@ -45,5 +45,12 @@ public class CustomerServiceImpl implements CustomerService {
 
 		customerDAO.deleteCustomer(theId);		
 	}
+	
+	@Override
+    @Transactional
+    public List<Customer> searchCustomers(String theSearchName) {
+
+        return customerDAO.searchCustomers(theSearchName);
+    }
 
 }
