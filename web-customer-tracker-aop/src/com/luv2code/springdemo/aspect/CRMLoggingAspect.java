258faf100af41f7c745124ca3d846e3cc2001544
Perego Daniele <peregopr@gmail.com>
2020-05-27 10:14:17 +0200

    Aggiornato aop project
    Arrivato lezione 359

--- web-customer-tracker-aop/src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
+++ web-customer-tracker-aop/src/com/luv2code/springdemo/aspect/CRMLoggingAspect.java
@@ -2,6 +2,7 @@ package com.luv2code.springdemo.aspect;
 
 import org.apache.log4j.Logger;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -38,10 +39,33 @@ public class CRMLoggingAspect {
 		
 		// display method argument
 		
+		// get the arguments
+		Object[] args = theJoinPoint.getArgs();
+		
+		// loop thru and display args
+		for(Object tempArg : args) {
+//			myLogger.info("=====>>> argumetn: " + tempArg);
+			System.out.println("=====>>> argument: " + tempArg);
+		}
 		
 	}
 	
 	
 	// add @AfterReturning advice
-	
+	@AfterReturning(
+			pointcut="forAppFlow()",
+			returning="theResult"
+			)
+	public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+		
+		// display method calling
+		String theMetod = theJoinPoint.getSignature().toShortString();
+		
+		System.out.println("======>> in @afterReturning: calling method: " + theMetod);
+		
+		// display data return
+		System.out.println("======>> result: " + theResult);
+				
+		
+	}
 }
